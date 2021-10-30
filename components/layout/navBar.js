ab26f340bec437678564577274db0dba57b8930f
misterZink <heidarirobin@protonmail.com>
2021-10-30 23:57:18 +0200

    Cart function is working

--- components/layout/navBar.js
+++ components/layout/navBar.js
@@ -4,8 +4,8 @@ export default function Navbar() {
     return (
         <nav style={{width: "100%", display: "flex", justifyContent: "space-around", fontSize: "2"}}>
             <Link href={"/"}>Dogshop</Link>
-            <Link href={"/about"}>Pick a Dog</Link>
-            <Link href={"/pokemon/pokemon"}>About</Link>
+            <Link href={"/pickadog"}>Pick a Dog</Link>
+            <Link href={"/varukorg"}>Basket</Link>
         </nav>
     )
 }
