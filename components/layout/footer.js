65ff02e35ddc6e33c2f42d3615341f86697ddebd
misterZink <heidarirobin@protonmail.com>
2021-10-31 00:50:58 +0200

    Navbar and footer changed colors and fixed positions

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,9 +1,7 @@
 export default function Footer() {
     return (
-        <footer style={{width: "100%", display: "flex", justifyContent: "space-around", fontSize: "2"}}>
-            This is our FOOTER!
-            <br/>
-            <br/>
+        <footer className="w-full flex justify-around text-lg h-24 items-center bg-blue-900 text-white mt-8">
+            <p className="text-xs">Copyright (c) 2021</p>
         </footer>
     )
 }
--- components/layout/navBar.js
+++ components/layout/navBar.js
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <nav style={{width: "100%", display: "flex", justifyContent: "space-around", fontSize: "2"}}>
-            <Link href={"/"}>Dogshop</Link>
-            <Link href={"/pickadog"}>Pick a Dog</Link>
-            <Link href={"/varukorg"}>Basket</Link>
+        <nav className="w-full flex justify-end  text-lg h-16 items-center pr-4 bg-blue-900 text-white">
+            <Link passHref href={"/"}><a className="rounded p-2 mr-4 cursor-pointer hover:bg-blue-600">Dogshop</a></Link>
+            <Link passHref href={"/pickadog"}><a className="rounded p-2 mr-4 cursor-pointer hover:bg-blue-600">Pick a dog</a></Link>
+            <Link passHref href={"/varukorg"}><a className="rounded p-2 mr-4 cursor-pointer hover:bg-blue-600">Basket</a></Link>
         </nav>
     )
 }
