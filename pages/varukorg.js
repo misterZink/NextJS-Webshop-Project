c12c4ff17e83f20e61af20ca148b8706cb3af546
Christian Pilz <svampodling@gmail.com>
2021-11-02 12:43:33 +0100

    Updated the look of client/card info

--- pages/varukorg.js
+++ pages/varukorg.js
@@ -19,7 +19,8 @@ export default function Varukorg() {
      * Updates what dogs the customer has in their basket.
      */
     function displayDogs() {
-        return dogs.map(dog => <div key={dog.dogName}><span className="block ...">{dog.dogName}&emsp;&emsp;&emsp;&emsp;{dog.price} :-</span></div>)
+        return dogs.map(dog => <div key={dog.dogName}><span
+            className="bg-gray-200 rounded block ...">{dog.dogName}&emsp;&emsp;&emsp;&emsp;{dog.price} :-</span></div>)
     }
 
     return (
@@ -34,14 +35,14 @@ export default function Varukorg() {
                 <h1 className="text-center mb-12 text-3xl">Varukorgen</h1>
             </div>
 
-            <h1 className="text-center mb-5 text-xl">Artiklar</h1>
+            <h1 className="text-center mb-1 text-xl">Artiklar</h1>
 
-            <div className="space-y-4 ...">
+            <div className="border-2 border-gray-100 rounded space-y-0.5 w-1/5 text-center ...">
                 {displayDogs()}
             </div>
 
             <div className="flex flex-col w-50% max-w-lg mb-5">
-                <h1 className="text-center mt-5 mb-5 text-3xl">Summa: {summ} kr</h1>
+                <h1 className="text-center mt-10 mb-5 text-3xl">Summa: {summ} kr</h1>
                 <button onClick={() => cart.cartDispatch({type: CART_ACTIONS.RESET_CART})}
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded content-center Link">
                     Töm varukorg
@@ -78,31 +79,28 @@ export default function Varukorg() {
                 </div>
 
                 <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                htmlFor="grid-phonenumber">
                             Telefonnummer
                         </label>
                         <input
                             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-phonenumber" type="text" placeholder="telefonnummer"/>
+                            id="grid-zip" type="text" placeholder="telefonnummer" max="256"/>
                     </div>
-                </div>
 
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                htmlFor="grid-adress">
                             Adress
                         </label>
                         <input
                             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-adress" type="text" placeholder="adress"/>
+                            id="grid-city" type="text" placeholder="adress" max="256"/>
                     </div>
                 </div>
 
                 <div className="flex flex-wrap -mx-3 mb-6">
-
                     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                htmlFor="grid-zip">
@@ -125,45 +123,42 @@ export default function Varukorg() {
                 </div>
 
                 <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                htmlFor="grid-cardnumber">
                             Kortnummer
                         </label>
                         <input
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-cardnumber" type="text" placeholder="**** **** **** ****" maxLength="16"/>
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-first-name" type="text" placeholder="**** **** **** ****" maxLength="16"/>
                         <p className="text-gray-600 text-xs italic">De 16 siffrorna på ditt korts framsida.</p>
                     </div>
-                </div>
 
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <div className="w-full md:w-1/6 px-3">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                htmlFor="grid-card-month">
                             Månad
                         </label>
                         <input
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-card-month" type="text" placeholder="**" maxLength="2"/>
-                        {/*<p className="text-red-500 text-xs italic">Please fill out this field.</p>*/}
+                            className="w-12 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-last-name" type="text" placeholder="**" maxLength="2"/>
                     </div>
-                    <div className="w-full md:w-1/2 px-3">
+
+                    <div className="w-full md:w-1/3 px-3">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                htmlFor="grid-card-year">
                             År
                         </label>
                         <input
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-card-year" type="text" placeholder="**" maxLength="2"/>
-                        {/*<p className="text-red-500 text-xs italic">Please fill out this field.</p>*/}
+                            className="w-12 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-last-name" type="text" placeholder="**" maxLength="2"/>
                     </div>
                 </div>
             </form>
 
             <div className="w-50% max-w-lg mb-10">
                 <button onClick={() => cart.cartDispatch({type: CART_ACTIONS.RESET_CART})}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded content-center Link">
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded content-center Link">
                     Genomför köp
                 </button>
             </div>
